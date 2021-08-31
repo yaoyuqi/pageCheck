0f7ac9898af5919afd145ed882756000a0724337
yaoyuqi <yaoyuq@gmail.com>
2021-08-31 22:04:53 +0800

    update

---
+++ src/main/java/Ywk/Main.java
@@ -0,0 +1,9 @@
+package Ywk;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String... args) {
+        Application.launch(MainApp.class, args);
+    }
+}
